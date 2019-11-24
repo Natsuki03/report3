c1490da9f24b221005bab527cb73523d3a7f6e89
Natsuki Oshiro <e195767@ie.u-ryukyu.ac.jp>
2019-11-24 12:27:16 +0900

    step1

--- src/main/jp/ac/uryukyu/ie/e195767/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e195767/Enemy.java
@@ -40,7 +40,13 @@ public class Enemy {
     public void attack(Hero hero){
         int damage = (int)(Math.random() * attack);
         System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-        hero.wounded(damage);
+        if(dead) {
+            damage = 0;
+            hero.wounded(damage);
+        }
+        else{
+            hero.wounded(damage);
+        }
     }
 
     /**
